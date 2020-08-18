b1b93c2338fffe840ed22c47be5c26ab6e8e6080
cjm3334 <cjm3334@tamu.edu>
2020-08-18 08:07:40 -0500

    Basic Functionality

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// Require discord.js package
+// Import discord.js
 const Discord = require("discord.js");
 
-// Create a new client using the new keyword
+// Create new client
 const client = new Discord.Client();
-
-// Display a message once the bot has started
 client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 // Check messages for a specific command
-client.on("message", msg =>{
-    // Send back a reply when the specific command has been written
-    if (msg.content === "!hello"){
-        msg.reply("Hello world!");
+client.on("message", message =>{
+
+    if (message.content.startsWith("!card")){
+        const args = message.content.slice(5).trim().replace(/\s/g, "-").replace(/'/g, "");
+        const cardName = args.toLowerCase();
+        const command = "https://argentsaga.com/product/" + cardName + "/"
+        message.reply(command);
     }
 });
 
 // Log in the bot with the token
-client.login("token");
+client.login("NzQ1MjU2NzY4MDg2NDc0Nzcy.XzvIcw.-dLE0j__eOH2Cib481pYdE_nmcE");
