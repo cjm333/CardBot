8c50d09b01b138892daac11ddf4b6340b763547e
cjm3334 <cjm3334@tamu.edu>
2020-08-26 23:40:10 -0500

    added specifics

--- app.js
+++ app.js
@@ -10,61 +10,85 @@ client.on("ready", () =>{
 
 // Check messages for a specific command
 client.on("message", message => {
-    const cardEmbed = new Discord.MessageEmbed()
-    channel = message.channel
-    var doCheck = new Boolean(true)
 
     if (message.content.startsWith("!card")){
-        const args = message.content.slice(5).trim()
-        const argsURL = args.replace(/\s/g, "-").replace(/'/g, "").replace(/,/g, "").replace(/#/g, "").replace(/:/g, "").replace(/&/g, "and").replace(/!/g, "and");
-        const cardName = argsURL.toLowerCase();
-
-        cardList = []
-        cardNames = []
-
-        for(card of data.cards){
-            if(card.append.includes(cardName)){
-                cardNames.push(card.name)
-                cardList.push(card)
-            }
-        }
-
-        if(cardList.length > 1){
-            for(card of cardList){
-                if(card.append === cardName){
-                    cardEmbed.setImage(card.url)
-                    cardEmbed.setTitle(card.name)
-                    cardEmbed.setDescription(card.effect)
-                    message.reply(cardEmbed);
-                    doCheck = false
-                }
-            }
-        }
-
-        if(doCheck){
-            if(cardList.length == 0){
-                message.reply("No cards exist with that phrase")
-            }
-            else if(cardList.length > 1 && cardList.length <= 10){
-                message.reply("Multiple cards match your phrase. Pick from the list below and try again:")
-                for(name of cardNames){
-                    channel.send(name)
-                }
-            }
-            else if(cardList.length > 10){
-                message.reply("Your search term was too broad. Be a bit more specific")
-            }
-            else if(cardList.length == 1){
-                cardEmbed.setImage(cardList[0].url)
-                cardEmbed.setTitle(cardList[0].name)
-                cardEmbed.setDescription(cardList[0].effect)
-                message.reply(cardEmbed);
-            }
-        }
-        
+        search(message, data.cards, 5)
+    }
+    else if(message.content.startsWith("!champion")){
+        search(message, data.champions, 9)
+    }
+    else if(message.content.startsWith("!spirit")){
+        search(message, data.spirits, 7)
+    }
+    else if(message.content.startsWith("!tower")){
+        search(message, data.towers, 6)
+    }
+    else if(message.content.startsWith("!shard")){
+        search(message, data.shards, 6)
+    }
+    else if(message.content.startsWith("!unit")){
+        search(message, data.units, 5)
+    }
+    else if(message.content.startsWith("!spell")){
+        search(message, data.spells, 6)
+    }
+    else if(message.content.startsWith("!augment")){
+        search(message, data.augments, 8)
     }
 
 });
 
+function search(message, selectedCards, trimLength){
+    const cardEmbed = new Discord.MessageEmbed()
+    channel = message.channel
+    var doCheck = new Boolean(true)
+    cardList = []
+    cardNames = []
+
+    const args = message.content.slice(trimLength).trim()
+    const argsURL = args.replace(/\s/g, "-").replace(/'/g, "").replace(/,/g, "").replace(/#/g, "").replace(/:/g, "").replace(/&/g, "and").replace(/!/g, "and");
+    const cardName = argsURL.toLowerCase();
+
+    for(card of selectedCards){
+        if(card.append.includes(cardName)){
+            cardNames.push(card.name)
+            cardList.push(card)
+        }
+    }
+
+    if(cardList.length > 1){
+        for(card of cardList){
+            if(card.append === cardName){
+                cardEmbed.setImage(card.url)
+                cardEmbed.setTitle(card.name)
+                cardEmbed.setDescription(card.effect)
+                message.reply(cardEmbed);
+                doCheck = false
+            }
+        }
+    }
+
+    if(doCheck){
+        if(cardList.length == 0){
+            message.reply("No cards exist with that phrase")
+        }
+        else if(cardList.length > 1 && cardList.length <= 10){
+            message.reply("Multiple cards match your phrase. Pick from the list below and try again:")
+            for(name of cardNames){
+                channel.send(name)
+            }
+        }
+        else if(cardList.length > 10){
+            message.reply("Your search term was too broad. Be a bit more specific")
+        }
+        else if(cardList.length == 1){
+            cardEmbed.setImage(cardList[0].url)
+            cardEmbed.setTitle(cardList[0].name)
+            cardEmbed.setDescription(cardList[0].effect)
+            message.reply(cardEmbed);
+        }
+    }
+}
+
 // Log in the bot with the token
-client.login("***");
+client.login("NzQ1MjU2NzY4MDg2NDc0Nzcy.XzvIcw.mRMOGEX1yJEBC7svjIEwV4KRMAc");
