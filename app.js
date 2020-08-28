36aa8d07597ebe860796e4bf59ec203e0f8f3a2b
cjm3334 <cjm3334@tamu.edu>
2020-08-28 10:33:16 -0500

    Various updates

--- app.js
+++ app.js
@@ -26,8 +26,46 @@ client.on("message", message => {
     else if(message.content.startsWith("!option")){
         search(message, data.options, 7)
     }
+    else if(message.content.startsWith("!alt")){
+        search(message, data.alt, 4)
+    }
     else if(message.content.startsWith("!count")){
         counting(message, data)
+    }
+    else if(message.content.startsWith("!count")){
+        message.reply("If you used any of the memory borrower skills aka  Metalgreymon/Weregarurumon/Chirinmon/Digitamamon or the option card Gravity Press, and decide to pass your turn while having positive memory, it goes like this:\n1) Declare you want to pass turn, so you put it to 3 on your opponent's side in order to pass the turn.\n2) Memory borrower's (end of turn) part of the effect activates, you get -3 or -2 memory for each memory borrower skill used, depending on which card you used")
+    }
+    else if(message.content.startsWith("!keyword")){
+        message.reply("Blocker: `When the opponent attacks, you may Rest this Active Digimon to become the target of that attack.`\n\n \
+Security Attack +/-X: `The number of Security cards checked by this Digimon +/- X. A player does not win the game when attacking directly with a Digimon with 0 Security Attack.`\n\n \
+Recovery +X: `Take X cards from the top of your deck and add them to the top of your Security.`\n\n \
+Piercing: `When this Digimon attacks and destroys an opponent’s Digimon by battle, it performs a Security Check against your opponent’s Security. This Security Check does not count as a direct attack.`\n\n \
+Draw +X: `Draw x cards from the top of your deck and add them to your hand.`\n\n \
+Jamming: `This Digimon cannot be destroyed in battle against a Security Digimon.`\n\n \
+De-Digivolve X: `Discard X cards from the top of a Digimon. If a Digimon has no Digivolution Sources or is the last Digimon in the stack, you may not continue discarding.`\n\n \
+Download -X: `When you would Digivolve into this Digimon, you may rest 1 of your Digimon to reduce the Digivolution Cost by X.`\n\n \
+Reboot: `In the opponent’s Active Phase, make this Digimon Active.`\n\n \
+Vengeance: `When this Digimon is destroyed in battle, destroy the Digimon it was battling.`").then(msg => {
+    msg.delete({ timeout: 60000 })})
+    }
+    else if(message.content.startsWith("!rulebook")){
+        message.reply("Your question can be answered by reading the rulebook. Please familiarize yourself with it, per the server rules, before you ask anymore questions. ")
+    }
+    else if(message.content.startsWith("!help")){
+        message.reply("Here are a list of commands:\n \
+!card: `Search all card names`\n \
+!digitama: `Search only Digitama cards`\n \
+!digimon: `Search only Digimon cards`\n \
+!tamer: `Search only Tamer cards`\n \
+!option: `Search only Option cards`\n \
+!alt: `Search for alternate arts`\n \
+!count: `Number of cards in a certain type`\n \
+!borrow: `Information on memory borrowing`\n \
+!keyword: `List keywords`\n \
+!rulebook: `Simple question answer`\n\n \
+Reminder: large messages self-delete after a short time")
+
+
     }
 
 });
@@ -40,7 +78,7 @@ function search(message, selectedCards, sliceLength){
     cardNames = []
 
     const args = message.content.slice(sliceLength).trim()
-    const argsURL = args.replace(/\s/g, "-").replace(/'/g, "")
+    const argsURL = args.replace(/\s/g, "-").replace(/'/g, "").replace("promo", "p").replace("(", "").replace(")", "")
     const cardName = argsURL.toLowerCase();
 
     for(card of selectedCards){
