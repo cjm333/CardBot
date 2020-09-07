ca1858d65b8f8af2ea2d7ed760f7477fedbd4991
cjm3334 <cjm3334@tamu.edu>
2020-09-07 01:35:15 -0500

    Welcome functionality

--- app.js
+++ app.js
@@ -14,6 +14,35 @@ client.on("ready", () =>{
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// User entrance message
+client.on("guildMemberAdd", member => {
+    digivice = getDigivice()
+    partner = getPartner()
+    client.channels.cache.get("681578269455548421").send("<@" + member.id + "> just joined and received a " + digivice + "! Their partner seems to be " + partner)
+})
+
+function getDigivice(){
+    digivices = ["Digivice", "D-3", "D-Power", "D-Tector", "Data Link", "Fusion Loader", "App Drive"]
+    index = Math.floor(Math.random() * 7)
+    return digivices[index]
+}
+
+function getPartner(){
+    partners = ["Agumon", "Gabumon", "Gomamon", "Tentomon", "Palmon", "Biyomon", "Patamon", "Salamon", "Betamon", 
+    "Guilmon", "Renamon", "Gatomon", "Lopmon", "Terriermon", "Gotsumon", "Kokuwamon", "DemiDevimon", "Monodramon", 
+    "FanBeemon", "Guardromon", "MarineAngemon", "Impmon", "Hagurumon", "Shoutmon", "Gumdramon", "Neemon", "Bearmon",
+    "Tapirmon", "Veemon", "Hawkmon", "Armadillomon", "Wormmon", "Meicoomon", "Calumon", "Leomon (Uh oh....)", 
+    "Kokomon", "Crabmon", "Airdramon", "Flarerizamon", "Centarumon", "Frigimon", "Tortomon", "Floramon", "Gizamon",
+    "Elecmon", "Syakomon", "Apemon", "Kuwagamon", "Snimon", "Unimon", "Gorillamon", "Dokamon", "Musimon", "Gatchmon",
+    "Hackmon", "Cyberdramon", "Gaomon", "Lalamon", "Falcomon", "Kudamon", "PawnChessmon (Black)", "PawnChessmon (White)",
+    "Hackmon (Appmon)", "BanchoLeomon", "Zeromaru", "Dorumon", "Argomon", "Keramon", "Kunemon", "Labramon", 
+    "BlackAgumon", "ToyAgumon", "Bokomon", "Monmon", "Morphomon", "BlackGabumon", "Muchomon", "Otamamon", "Penguinmon",
+    "DemiMeramon", "Phascomon", "Dracmon", "Dracomon", "Psychemon", "Burgermon", "Goblimon", "Gazimon", "Solarmon",
+    "SnowAgumon", "Tinkermon", "Vorvomon", "Kamemon", "Lucemon", "Jazamon", "Zubamon", "Chuumon", "Lunamon", "uh....... Coronamon....."]
+    index = Math.floor(Math.random() * partners.length)
+    return partners[index]
+}
+
 // Check messages for a specific command
 client.on("message", message => {
 
