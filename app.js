2bbb68d8d3b77c7cd26f0ea3d0b97eab2dbc060e
cjm3334 <cjm3334@tamu.edu>
2020-09-11 21:03:42 -0500

    Typo fixes

--- app.js
+++ app.js
@@ -90,10 +90,10 @@ Vengeance: `When this Digimon is destroyed in battle, destroy the Digimon it was
         message.channel.send("Your question can be answered by reading the rulebook. Please familiarize yourself with it, per the server rules, before you ask anymore questions:\nhttps://drive.google.com/folderview?id=13l4AygyBAPDxul58Uin4sczHNmEhaSOF")
     }
     else if(message.content.startsWith("!faq")){
-        message.channel.send("1. Play/Digivolve: `Playing and digivolving are NOT the same thing. These cause different effects and cost different amounts on cards`\n \
-        2. Raising Area: `Digimon in the raising area cannot activate their effects and cannot have effects affect them UNLESS specifically mentioned on the card in question`\n \
-        3. Effects Transfer: `If you evolve a mon with an effect (+DP, +Security, etc), it applies to evolved mon as well. This includes summoning sickness. NOTE: Digimon moved from raising to play do NOT have summoning sickness` \n \
-        4. Color Restrictions: `Tamers have no color restrictions. Digimon have color restrictions for evolving, the color of which is specified in the bubble (two colored bubbles indicates \"pick one\"). Options require a tamer or digimon of the same color to be on the field (this INCLUDES raising area).`")
+        message.channel.send("1. Play/Digivolve: `Playing and digivolving are NOT the same thing. These cause different effects and cost different amounts on card`\n \
+2. Raising Area: `Digimon in the raising area cannot activate their effects and cannot have effects affect them UNLESS specifically mentioned on the card in question`\n \
+3. Effect Transfer: `If you evolve a mon with an effect (+DP, +Security, etc), it applies to evolved mon as well. This includes summoning sickness. NOTE: Digimon moved from raising to play do NOT have summoning sickness` \n \
+4. Color Restrictions: `Tamers have no color restrictions. Digimon have color restrictions for evolving, the color of which is specified in the bubble (two colored bubbles indicates \"pick one\"). Options require a tamer or digimon of the same color to be on the field (this INCLUDES raising area).`")
     }
     else if(message.content.startsWith("!help")){
         message.reply("Here are a list of commands:\n \
