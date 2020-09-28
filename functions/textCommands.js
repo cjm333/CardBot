ba3f2f5986a519655be18887d3091f0f06f574d4
Carter Mitchell <cjmitchell333@gmail.com>
2020-09-28 12:12:56 -0500

    Help fix

--- functions/textCommands.js
+++ functions/textCommands.js
@@ -1,17 +1,17 @@
 module.exports = {
     help: function(message){
         message.reply("Here are a list of commands:\n \
-!card:: `Search all cards`\n \
-!unit:: `Search only units`\n \
-!spell:: `Search only spells`\n \
-!augment:: `Search only augments`\n \
+!card: `Search all cards`\n \
+!unit: `Search only units`\n \
+!spell: `Search only spells`\n \
+!augment: `Search only augments`\n \
 !champion: `Search only champions`\n \
 !spirit: `Search only spirits`\n \
 !tower: `Search only towers`\n \
-!shard:: `Search only shards`\n \
-!count:: `Get how many cards there are of a type`\n \
-!element:: `Search in a particular element`\n \
+!shard: `Search only shards`\n \
+!count: `Get how many cards there are of a type`\n \
+!element: `Search in a particular element`\n \
 !basic: `List all basic units in the game`\n \
-Append 'Type' or 'Text' to a command with :: to search for card with a certain type. Ex: !unitType, !spiritText")
+Append 'Type' or 'Text' to a command (besides !basic) to filter based on preference. Ex: !unitType, !spiritText")
     }
 }
