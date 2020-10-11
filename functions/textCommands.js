bd7321f0a528bb34d49990e4ede147c962f59faf
cjm3334 <cjm3334@tamu.edu>
2020-10-11 01:27:26 -0500

    events update

--- functions/textCommands.js
+++ functions/textCommands.js
@@ -115,12 +115,12 @@ Digitama Deck:\n \
     },
     event: function(message){
         message.channel.send("Upcoming Events:\n \
-`1. October 10th, 5pm UK Time: London Players Guild Tourney, Hosted by LPG -` https://challonge.com/vc4z8f6s\n\n \
+`1. October 17th, 3pm BST/10AM EST Extreme Game by Antinomy`\n\n \
 `2. October 25th, 11am UK Time: London Players Guild Tourney, Hosted by LPG -` https://challonge.com/y1uw3wkk\n\n \
 `3. October 26th, 10am CDT (Week Long Event): DigiRumble 2, Hosted by orangeswim -` https://challonge.com/dcdevR2\n\n \
 `4. October 31st: Big Byte #7, Hosted by RazeOblivion`\n\n \
 `5. November 1st, 7pm EST: DigiDuels, Hosted by Drago -` https://challonge.com/DigiDuels\n\n \
-`6. TBD: Teams Event by Antinomy`")
+`6. TBD: Tournament by Nightmare`")
     }
 
 }
