c524d784bd9d2269bb6f20082d0483fc8262f343
Carter Mitchell <cjmitchell333@gmail.com>
2020-10-13 13:09:42 -0500

    Updates

--- functions/textCommands.js
+++ functions/textCommands.js
@@ -117,10 +117,11 @@ Digitama Deck:\n \
         message.channel.send("Upcoming Events:\n \
 `1. October 17th, 3pm BST/10AM EST Extreme Game by Antinomy`\n\n \
 `2. October 25th, 11am UK Time: London Players Guild Tourney, Hosted by LPG -` https://challonge.com/y1uw3wkk\n\n \
-`3. October 26th, 10am CDT (Week Long Event): DigiRumble 2, Hosted by orangeswim -` https://challonge.com/dcdevR2\n\n \
+`3. October 26th-30th, 10am CDT: DigiRumble 2, Hosted by orangeswim -` https://challonge.com/dcdevR2\n\n \
 `4. November 1st, 7pm EST: DigiDuels, Hosted by Drago -` https://challonge.com/DigiDuels\n\n \
-`5. November 7th: Big Byte #7, Hosted by RazeOblivion`\n\n \
-`6. TBD: Tournament by Nightmare`")
+`5. November 4th-6th: DigiRumble 2 Finals`\n\n \
+`6. November 7th: Big Byte #7, Hosted by RazeOblivion`\n\n \
+`7. TBD: Tournament by Nightmare`")
     }
 
 }
