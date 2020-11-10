5602be58b60234378b82b91ef5f4cb0e0a4e26dd
Carter Mitchell <cjmitchell333@gmail.com>
2020-11-10 11:22:55 -0600

    Prevention

--- app.js
+++ app.js
@@ -86,44 +86,48 @@ client.on("guildMemberAdd", member => {
 
 //Check messages for commands
 client.on("message", message => {
-
-    if (message.content.startsWith("!card")){search.searching(message, allCards, 5, Discord);}
-    else if(message.content.startsWith("!digitama")){search.searching(message, digitama.digitama, 9, Discord);}
-    else if(message.content.startsWith("!digimon")){search.searching(message, digimon.digimon, 8, Discord);}
-    else if(message.content.startsWith("!tamer")){search.searching(message, tamers.tamers, 7, Discord);}
-    else if(message.content.startsWith("!option")){search.searching(message, options.options, 7, Discord);}
-    else if(message.content.startsWith("!alt")){search.searching(message, altArts.alt, 4, Discord);}
-    else if(message.content.startsWith("!reference")){textCommands.reference(message, bonus);}
-    else if(message.content.startsWith("!borrow")){textCommands.borrow(message);}
-    else if(message.content.startsWith("!count")){count.counting(message, stats);}
-    else if(message.content.startsWith("!faq")){textCommands.faq(message);}
-    else if(message.content.startsWith("!help")){textCommands.help(message);}
-    else if(message.content.startsWith("!keyword")){textCommands.keyword(message);}
-    else if(message.content.startsWith("!partner")){
-        if(message.member.roles.cache.has('774145809280925736')){message.reply("Sorry, your partner abandoned you because you were ungrateful to them");}
-        else if(message.content.includes("change") || message.content.includes("Change")){
-            message.reply("You and your partner are bonded for life! You'd better appreciate them!");
-            message.member.roles.add('774145809280925736')
+    if(message.guild.id == "681578268729540663" || message.guild.id == "709829599604768770" || message.guild.id == "770014933474607196"){
+        if (message.content.startsWith("!card")){search.searching(message, allCards, 5, Discord);}
+        else if(message.content.startsWith("!digitama")){search.searching(message, digitama.digitama, 9, Discord);}
+        else if(message.content.startsWith("!digimon")){search.searching(message, digimon.digimon, 8, Discord);}
+        else if(message.content.startsWith("!tamer")){search.searching(message, tamers.tamers, 7, Discord);}
+        else if(message.content.startsWith("!option")){search.searching(message, options.options, 7, Discord);}
+        else if(message.content.startsWith("!alt")){search.searching(message, altArts.alt, 4, Discord);}
+        else if(message.content.startsWith("!reference")){textCommands.reference(message, bonus);}
+        else if(message.content.startsWith("!borrow")){textCommands.borrow(message);}
+        else if(message.content.startsWith("!count")){count.counting(message, stats);}
+        else if(message.content.startsWith("!faq")){textCommands.faq(message);}
+        else if(message.content.startsWith("!help")){textCommands.help(message);}
+        else if(message.content.startsWith("!keyword")){textCommands.keyword(message);}
+        else if(message.content.startsWith("!partner")){
+            if(message.member.roles.cache.has('774145809280925736')){message.reply("Sorry, your partner abandoned you because you were ungrateful to them");}
+            else if(message.content.includes("change") || message.content.includes("Change")){
+                message.reply("You and your partner are bonded for life! You'd better appreciate them!");
+                message.member.roles.add('774145809280925736')
+            }
+            else{partnerFunctions.partner(message, partners, tracking);}
         }
-        else{partnerFunctions.partner(message, partners, tracking);}
+        else if(message.content.startsWith("!rulebook")){textCommands.rulebook(message);}
+        else if(message.content.startsWith("!meme")){
+            const cardEmbed = new Discord.MessageEmbed()
+            cardEmbed.setImage("https://digimon-bucket.s3.amazonaws.com/meme.png")
+            message.reply(cardEmbed)
+        }
+        else if(message.content.startsWith("!starterRed")){textCommands.red(message);}
+        else if(message.content.startsWith("!starterBlue")){textCommands.blue(message);}
+        else if(message.content.startsWith("!starterYellow")){textCommands.yellow(message);}
+        else if(message.content.startsWith("!starterGreen")){textCommands.green(message);}
+        else if(message.content.startsWith("!starterBlack")){textCommands.black(message);}
+        else if(message.content.startsWith("!starterPurple")){textCommands.purple(message);}
+        else if(message.content.startsWith("!event")){textCommands.event(message);}
+        else if(message.content.startsWith("!info")){textCommands.info(message);}
+        else if(message.content.startsWith("!digivolve")){partnerFunctions.digivolve(message, partners, tracking);}
+        else if(message.content.startsWith("!dedigivolve")){partnerFunctions.dedigivolve(message, partners, tracking);}
+        else if(message.content.startsWith("!fight")){partnerFunctions.fight(message, partners, tracking);}
     }
-    else if(message.content.startsWith("!rulebook")){textCommands.rulebook(message);}
-    else if(message.content.startsWith("!meme")){
-        const cardEmbed = new Discord.MessageEmbed()
-        cardEmbed.setImage("https://digimon-bucket.s3.amazonaws.com/meme.png")
-        message.reply(cardEmbed)
+    else{
+        message.reply("Sorry, this server is not approved for bot usage. Please contact Shogunate to gain approval")
     }
-    else if(message.content.startsWith("!starterRed")){textCommands.red(message);}
-    else if(message.content.startsWith("!starterBlue")){textCommands.blue(message);}
-    else if(message.content.startsWith("!starterYellow")){textCommands.yellow(message);}
-    else if(message.content.startsWith("!starterGreen")){textCommands.green(message);}
-    else if(message.content.startsWith("!starterBlack")){textCommands.black(message);}
-    else if(message.content.startsWith("!starterPurple")){textCommands.purple(message);}
-    else if(message.content.startsWith("!event")){textCommands.event(message);}
-    else if(message.content.startsWith("!info")){textCommands.info(message);}
-    else if(message.content.startsWith("!digivolve")){partnerFunctions.digivolve(message, partners, tracking);}
-    else if(message.content.startsWith("!dedigivolve")){partnerFunctions.dedigivolve(message, partners, tracking);}
-    else if(message.content.startsWith("!fight")){partnerFunctions.fight(message, partners, tracking);}
 });
 
 // Log in the bot with the token
