7b781efd52a106ce6b78e543e2724c41f8a0d0f9
Carter Mitchell <cjmitchell333@gmail.com>
2020-12-26 14:57:27 -0600

    Partner updates

--- functions/partner.js
+++ functions/partner.js
@@ -99,6 +99,7 @@ module.exports = {
                 else if(elitePartner == "Gargomon"){elitePartner = "galgomon";}
                 else if(elitePartner == "Ogudomon X"){elitePartner = "ogudomon_x";}
                 else if(elitePartner == "Diablomon X"){elitePartner = "diablomon_x";}
+                else if(elitePartner == "OmniShoutmon"){elitePartner = "omegashoutmon";}
                 else if(elitePartner.includes("Millenniummon")){elitePartner = elitePartner.replace("Millenniummon", "Millenniumon");}
                 else if(elitePartner.includes("Hackmon")){elitePartner = elitePartner.replace("Hackmon", "Huckmon");}
                 else if(elitePartner == "an Egg" || elitePartner == "TerrierCrewsmon" || elitePartner == "ModokiBetamon"){customImage = true;}
